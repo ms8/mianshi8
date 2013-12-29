3d649e846159f089de9a4fc6f86dc15e2d73acf1
guhuazhen <gqlshare@163.com>
2013-12-29 23:20:23 +0800

    guhuazhen 首页增加百度验证

--- protected/views/layouts/site.php
+++ protected/views/layouts/site.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<meta name="baidu-tc-verification" content="79c93ec20588e733d75ac2a10b35e448" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="robots" content="all" />
 <meta name="author" content="mianshi8@qq.com" />
